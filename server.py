1592dd11dae2e2703f3adf0f6837d08752905e04
Harshith Varma <harshitvarma638@gmail.com>
2023-12-02 20:57:58 +0530

    testing

--- server.py
+++ server.py
@@ -27,24 +27,23 @@ def process_image(image_path):
 
         result = image_analyzer.analyze()
         text_output = ['hello','world']
-        return text_output
-        # if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
-        #     if result.text is not None:
-        #         for line in result.text.lines:
-        #             decoded_line = ""
-        #             for char in line.content:
-        #                 if isinstance(char, bytes):
-        #                     decoded_line += char.decode('utf-8', errors='replace')
-        #                 else:
-        #                     decoded_line += char
-        #             text_output.append(decoded_line)
-        #         return text_output
-        #     else:
-        #         error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
-        #         print("Analysis failed.")
-        #         print("Error reason: {}".format(error_details.reason))
-        #         print("Error code: {}".format(error_details.error_code))
-        #         print("Error message: {}".format(error_details.message))
+        if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+            if result.text is not None:
+                for line in result.text.lines:
+                    decoded_line = ""
+                    for char in line.content:
+                        if isinstance(char, bytes):
+                            decoded_line += char.decode('utf-8', errors='replace')
+                        else:
+                            decoded_line += char
+                    text_output.append(decoded_line)
+                return text_output
+            else:
+                error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+                print("Analysis failed.")
+                print("Error reason: {}".format(error_details.reason))
+                print("Error code: {}".format(error_details.error_code))
+                print("Error message: {}".format(error_details.message))
     except Exception as e:
         print("Error processing image:", str(e))
         return jsonify({'error': 'Failed to process image'})
