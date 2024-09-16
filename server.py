9344e985d5592d5124be64915ecbbac84ce0a0fc
PINNAMARAJU HARSHITH VARMA <102041932+harshitvarma638@users.noreply.github.com>
2024-09-16 12:47:33 +0530

    Update server.py
    added CORS policy

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import azure.ai.vision as sdk
 import tempfile
 import requests
 
 app = Flask(__name__)
+CORS(app)
 port = int(os.environ.get("PORT", 5000))
 service_options = sdk.VisionServiceOptions(os.environ['VISION_ENDPOINT'], os.environ['VISION_KEY'])
 
